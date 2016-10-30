d25ea777e19d7606714663563b4ca63dd23b1b62
HevLfreis <hevlhayt@foxmail.com>
2016-10-30 22:03:51 +0800

    update zh_cn iauth, home tp

--- LineMe/settings.py
+++ LineMe/settings.py
@@ -75,8 +75,7 @@ ROOT_URLCONF = 'LineMe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
